78eab805394c3b2cebf88cf5ef7217a35c38c720
Verteniasty <yayouyayou901@gmail.com>
2026-09-24 20:15:43 +0200

    Wypełnienie danych testowych do pliku bazy danych

--- database/database.sql
+++ database/database.sql
@@ -10,7 +10,7 @@ CREATE TABLE uzytkownicy (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     imie VARCHAR(40) NOT NULL,
     nazwisko VARCHAR(40) NOT NULL,
-    email VARCHAR(60) NOT NULL,
+    email VARCHAR(60) UNIQUE NOT NULL,
     haslo VARCHAR(255) NOT NULL,
     numer_telefonu VARCHAR(30) NOT NULL,
     rola VARCHAR(50) NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE uslugi (
 
 CREATE TABLE pracownicy (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_uzytkownika INT NOT NULL,
+    id_uzytkownika INT NOT NULL UNIQUE,
     opis VARCHAR(255),
     aktywne BOOLEAN NOT NULL DEFAULT TRUE,
 
@@ -72,7 +72,7 @@ CREATE TABLE rezerwacje (
     data_rezerwacji DATE NOT NULL,
     godzina_rozpoczecia TIME NOT NULL,
     godzina_zakonczenia TIME NOT NULL,
-    status VARCHAR(30) NOT NULL DEFAULT 'oczekujace',
+    status ENUM("oczekujace", "potwierdzone", "anulowane", "zakonczone") NOT NULL DEFAULT 'oczekujace',
     komentarz VARCHAR(512) NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
@@ -81,7 +81,43 @@ CREATE TABLE rezerwacje (
     FOREIGN KEY (id_uslugi) REFERENCES uslugi(id) ON DELETE CASCADE
 );
 
+CREATE INDEX idx_rezerwacje_data ON rezerwacje(data_rezerwacji);
+
 -- e1dddc844ca8ad19718295dbf2f0ed6746b459c2e3582ef8bf909812a24d9fe7 = test123!
 
-INSERT INTO uzytkownicy (imie, nazwisko, email, haslo, numer_telefonu, rola)
-VALUES ("Tomasz", "Kuzicki", "Tomasz.Kuzicki@gmail.com", "e1dddc844ca8ad19718295dbf2f0ed6746b459c2e3582ef8bf909812a24d9fe7", "+48203591342", "użytkownik")
+INSERT INTO uzytkownicy (imie, nazwisko, email, haslo, numer_telefonu, rola) VALUES
+("Tomasz", "Kuzicki", "Tomasz.Kuzicki@gmail.com", "e1dddc844ca8ad19718295dbf2f0ed6746b459c2e3582ef8bf909812a24d9fe7", "+48203591342", "użytkownik"),
+("Anna", "Nowak", "Anna.Nowak@gmail.com", "e1dddc844ca8ad19718295dbf2f0ed6746b459c2e3582ef8bf909812a24d9fe7", "+48493582532", "pracownik"),
+("Kamil", "Woźnica", "Kamil.Woznica@gmail.com", "e1dddc844ca8ad19718295dbf2f0ed6746b459c2e3582ef8bf909812a24d9fe7", "+48624593201", "pracownik"),
+("Karolina", "Celin", "Karolina.Celin@gmail.com", "e1dddc844ca8ad19718295dbf2f0ed6746b459c2e3582ef8bf909812a24d9fe7", "+48734593428", "użytkownik");
+
+INSERT INTO kategorie_uslug (nazwa, opis) VALUES
+("Samochody Osobowe", "Ekonomiczne i komfortowe auta miejskie oraz kompaktowe."),
+("SUV / Crossover", "Przestronne auta z napędem 4x4 na każdą trasę i rodzinne wyjazdy."),
+("Samochody Sportowe / Premium", "Ekskluzywne auta o wysokich osiągach na specjalne okazje.");
+
+INSERT INTO uslugi (id_kategorii, nazwa, opis, czas_trwania, cena) VALUES 
+(1, 'Toyota Corolla (Doba)', 'Najpopularniejszy sedan, hybryda, automatyczna skrzynia biegów.', 1440, 180.00),
+(1, 'Skoda Fabia (Doba)', 'Ekonomiczne auto miejskie, idealne na codzienne dojazdy.', 1440, 130.00),
+(2, 'Kia Sportage (Doba)', 'Nowoczesny SUV z bogatym wyposażeniem i napędem 4x4.', 1440, 320.00),
+(3, 'BMW Serii 4 Coupe (Doba)', 'Sportowe coupe o dynamicznym charakterze i luksusowym wnętrzu.', 1440, 650.00);
+
+INSERT INTO pracownicy (id_uzytkownika, opis, aktywne) VALUES
+(2, "Specjalista ds. obsługi klienta i wydań floty osobowej.", TRUE),
+(3, "Kierownik floty aut premium i sportowych", TRUE);
+
+INSERT INTO uslugi_pracownikow (id_pracownika, id_uslugi) VALUES
+(1,1),
+(1,2),
+(1,3),
+(2,4);
+
+INSERT INTO godziny_pracy_pracownikow (id_pracownika, dzien_tygodnia, data, godzina_rozpoczecia, godzina_zakonczenia) VALUES
+(1, 1, NULL, "08:00:00", "18:00:00"),
+(1, 2, NULL, "08:00:00", "18:00:00"),
+(2, 5, NULL, "09:00:00", "17:00:00"),
+(2, 6, NULL, "09:00:00", "14:00:00");
+
+INSERT INTO rezerwacje (id_uzytkownika, id_pracownika, id_uslugi, data_rezerwacji, godzina_rozpoczecia, godzina_zakonczenia, status, komentarz) VALUES
+(1, 1, 1, "2026-06-10", "10:00:00", "10:00:00", "potwierdzone", "Proszę o przygotowanie fotelika dziecięcego."),
+(4, 2, 4, "2026-06-15", "14:00:00", "14:00:00", "oczekujace", "Wynajem na weekend ślubny.");
